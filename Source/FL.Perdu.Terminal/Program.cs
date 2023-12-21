a489e5a6daddda752b57566cf6cc4788de26fa92
felip3fl <felip3.fl@gmail.com>
2023-12-21 12:40:14 -0300

    Start using CMD inside project

--- Source/FL.Perdu.Terminal/Program.cs
+++ Source/FL.Perdu.Terminal/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var destination = "%userprofile%\\OneDrive\\Documents\\Backup\\Games\\Metal gear rising\\";
+var origin = "%userprofile%\\Documents\\MGR\\";
+var fileName = $"metal_gear_rising_backup_{DateTime.Now.ToString("yyyyMMdd")}_{DateTime.Now.ToString("HHmm")}.7z";
+
+string strCmdText;
+strCmdText = $"/C .\\7z.exe a -t7z {destination}temp.7z \"{origin}*\" -mx=9";
+Console.WriteLine(strCmdText);
+Console.WriteLine(fileName);
+//System.Diagnostics.Process.Start("CMD.exe", strCmdText);
