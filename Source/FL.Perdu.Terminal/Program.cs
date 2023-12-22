52c4edb150c1df6f80b5b0912a0d0546b882c20d
felip3fl <felip3.fl@gmail.com>
2023-12-22 16:41:40 -0300

    Update code

--- Source/FL.Perdu.Terminal/Program.cs
+++ Source/FL.Perdu.Terminal/Program.cs
@@ -3,7 +3,7 @@ var origin = "%userprofile%\\Documents\\MGR\\";
 var fileName = $"metal_gear_rising_backup_{DateTime.Now.ToString("yyyyMMdd")}_{DateTime.Now.ToString("HHmm")}.7z";
 
 string strCmdText;
-strCmdText = $"/C .\\7z.exe a -t7z {destination}temp.7z \"{origin}*\" -mx=9";
+strCmdText = $"/C cd \"C:\\Program Files\\7-Zip\\; 7z.exe\" a -t7z {destination}temp.7z \"{origin}*\" -mx=9";
 Console.WriteLine(strCmdText);
 Console.WriteLine(fileName);
-//System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+System.Diagnostics.Process.Start("powershell.exe", strCmdText);
