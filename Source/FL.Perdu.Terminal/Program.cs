add3690f4d0b8acde23c37fbec42961c1caca270
felip3fl <felip3.fl@gmail.com>
2023-12-23 16:02:49 -0300

    Using 7zip to compact

--- Source/FL.Perdu.Terminal/Program.cs
+++ Source/FL.Perdu.Terminal/Program.cs
@@ -1,9 +1,16 @@
-﻿var destination = "%userprofile%\\OneDrive\\Documents\\Backup\\Games\\Metal gear rising\\";
-var origin = "%userprofile%\\Documents\\MGR\\";
+﻿using System.Text;
+
+var destination = @"%userprofile%\OneDrive\Documents\Backup\Games\Metal gear rising\";
+var origin = @"%userprofile%\Documents\MGR\";
 var fileName = $"metal_gear_rising_backup_{DateTime.Now.ToString("yyyyMMdd")}_{DateTime.Now.ToString("HHmm")}.7z";
 
-string strCmdText;
-strCmdText = $"/C cd \"C:\\Program Files\\7-Zip\\; 7z.exe\" a -t7z {destination}temp.7z \"{origin}*\" -mx=9";
-Console.WriteLine(strCmdText);
-Console.WriteLine(fileName);
-System.Diagnostics.Process.Start("powershell.exe", strCmdText);
+destination = destination.Replace("%userprofile%", System.Environment.GetEnvironmentVariable("USERPROFILE"));
+origin = origin.Replace("%userprofile%", System.Environment.GetEnvironmentVariable("USERPROFILE"));
+
+System.Diagnostics.Process process = new System.Diagnostics.Process();
+System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+startInfo.FileName = @"C:\Program Files\7-Zip\7z.exe";
+startInfo.Arguments = $"a -t7z \"{destination}{fileName}\" \"{origin}\"* -mx=9";
+process.StartInfo = startInfo;
+process.Start();
