58a6e8aca2edccb2f5bb7068eed7ef7429e64449
FL <felip3.fl@gmail.com>
2024-01-01 17:47:12 -0300

    New file Program model

---
+++ Source/FL.Perdu.Model/ProgramDetail.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace FL.Perdu.Model
+{
+    public class ProgramDetail
+    {
+        public string Name {  get; set; }
+        public string Address { get; set; }
+        public string BackupAddress { get; set; }
+        public int Order { get; set; }
+    }
+}
