244ab25b6a262d44ddb9a9308fd0e01b9affc304
FL <felip3.fl@gmail.com>
2024-01-01 17:49:50 -0300

    New Start class (Terminal)

---
+++ Source/FL.Perdu.Terminal/Start.cs
@@ -0,0 +1,31 @@
+﻿using FL.Perdu.Work.Services;
+
+namespace FL.Perdu.Terminal
+{
+    public class Start
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("FLex Perdu v1.0");
+            ShowOption();
+
+            //BackupService backupService = new();
+            //var isSuccess = backupService.Backup();
+
+            LocalFileService localFileService = new();
+            localFileService.LoadProgramDetail();
+
+            foreach (var item in localFileService.programDetails)
+            {
+                Console.WriteLine($"[{item.Order}] {item.Name}");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void ShowOption()
+        {
+            Console.WriteLine("Please select the follow options: ");
+        }
+    }
+}
