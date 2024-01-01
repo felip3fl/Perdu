3cec4746699d0d5d9dd711fea04e30d28358a6df
FL <felip3.fl@gmail.com>
2024-01-01 17:48:31 -0300

    New CSV project

---
+++ Source/FL.Perdu.Csv/LocalFile.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace FL.Perdu.Csv
+{
+    public class LocalFile
+    {
+        public void ReadCsvAsync(string address = "root")
+        {
+
+        }
+    }
+}
