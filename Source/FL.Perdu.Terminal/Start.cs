5c0e103e79fc7d5fa0364933006af5a6690c18cf
FL <felip3.fl@gmail.com>
2024-01-02 08:20:40 -0300

    Parameterized Backup method

--- Source/FL.Perdu.Terminal/Start.cs
+++ Source/FL.Perdu.Terminal/Start.cs
@@ -9,23 +9,26 @@ namespace FL.Perdu.Terminal
             Console.WriteLine("FLex Perdu v1.0");
             ShowOption();
 
-            //BackupService backupService = new();
-            //var isSuccess = backupService.Backup();
-
             LocalFileService localFileService = new();
             localFileService.LoadProgramDetail();
 
+            Console.WriteLine("Please select the follow options: ");
             foreach (var item in localFileService.programDetails)
             {
                 Console.WriteLine($"[{item.Order.ToString("00")}] {item.Name}");
             }
 
-            Console.ReadKey();
+            Console.Write("\nYour choice: ");
+
+            var userOption = Convert.ToInt32(Console.ReadLine());
+
+            BackupService backupService = new();
+            var isSuccess = backupService.Backup(localFileService.programDetails[userOption-1]);
         }
 
         private static void ShowOption()
         {
-            Console.WriteLine("Please select the follow options: ");
+            
         }
     }
 }
--- Source/FL.Perdu.Work/Services/BackupService.cs
+++ Source/FL.Perdu.Work/Services/BackupService.cs
@@ -1,17 +1,18 @@
-﻿using FL.Perdu.SevenZip;
+﻿using FL.Perdu.Model;
+using FL.Perdu.SevenZip;
 using FL.Perdu.Work.Interfaces;
 
 namespace FL.Perdu.Work.Services
 {
     public class BackupService : IBackupService
     {
-        public async Task Backup()
+        public async Task Backup(ProgramDetail programDetail)
         {
             SevenZipPrompt sevenZip = new SevenZipPrompt();
 
-            var destination = @"%userprofile%\OneDrive\Documents\Backup\Games\Metal gear rising\";
-            var origin = @"%userprofile%\Documents\MGR\";
-            var fileName = $"metal_gear_rising_backup_{DateTime.Now.ToString("yyyyMMdd")}_{DateTime.Now.ToString("HHmm")}.7z";
+            var destination = programDetail.BackupAddress;
+            var origin = programDetail.Address;
+            var fileName = $"{programDetail.Name.Replace(" ","_")}_{DateTime.Now.ToString("yyyyMMdd")}_{DateTime.Now.ToString("HHmm")}.7z";
             var userAddress = System.Environment.GetEnvironmentVariable("USERPROFILE");
 
             destination = destination.Replace("%userprofile%", userAddress);
